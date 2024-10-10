5e7a7532e0e07c1a6f6db3d1ca80e3c4beaf3d05
m.belloli <michele.belloli@gmail.com>
2024-10-10 23:50:02 +0200

    Load from video file

--- main.py
+++ main.py
@@ -2,12 +2,29 @@
 
 import cv2
 
+CAMERA_INPUT = False
+INPUT_VIDEO = 'output.mp4'
+
+def process_frame(frame):
+    o = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return o
+"""
+ people identification and segmentation
+ position per person
+ map calibration
+ send position to the map
+"""
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Starting Computer Vision Project...')
 
     # Open the default camera
-    cam = cv2.VideoCapture(0)
+    if CAMERA_INPUT:
+        cam = cv2.VideoCapture(0)
+    else:
+        cam = cv2.VideoCapture(INPUT_VIDEO)
 
     # Get the default frame width and height
     frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -17,15 +34,14 @@ if __name__ == '__main__':
     #fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     #out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
-    while True:
+    while CAMERA_INPUT or cam.isOpened():
         ret, frame = cam.read()
 
         out_frame = process_frame(frame)
-        cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        out.write(frame)
+        #out.write(frame)
         # Display the captured frame
-        cv2.imshow('Camera', gray_frame)
+        cv2.imshow('Camera', out_frame)
 
         # Press 'q' to exit the loop
         if cv2.waitKey(1) == ord('q'):

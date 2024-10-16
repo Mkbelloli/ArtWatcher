15e0f50e8a899848c16436a4d0babddc77a0710e
m.belloli <michele.belloli@gmail.com>
2024-10-16 15:29:22 +0200

    Smoothed point movement

--- Viewer.py
+++ Viewer.py
@@ -2,24 +2,36 @@ import pyglet
 import socket
 import threading
 import random
+import time
 from pyglet import shapes
+
+PIXEL_PER_METER = 40
+X_MARGIN = 10
+Y_MARGIN = 10
+MAX_IDLE_TIME = 3
+
 class PeopleTrackingUI:
     def __init__(self, width, height):
-        self.window = pyglet.window.Window( width, height)
+        self.__width = width * PIXEL_PER_METER + 2 * X_MARGIN
+        self.__height = height * PIXEL_PER_METER + 2 * Y_MARGIN
+        self.window = pyglet.window.Window( self.__width, self.__height)
         self.batch = pyglet.graphics.Batch()
-        self.__width = width
-        self.__height = height
-        self.__x_margin = 10
-        self.__y_margin = 10
-        self.__base = shapes.Rectangle(self.__x_margin, self.__y_margin, width-2*self.__x_margin, height--2*self.__y_margin, color=(30, 75, 70), batch=self.batch)
+        self.__base = shapes.Rectangle(X_MARGIN, Y_MARGIN, self.__width-2*X_MARGIN, self.__height-2*Y_MARGIN, color=(30, 75, 70), batch=self.batch)
+
         self.window.push_handlers(self.on_draw)
         self.__people = {}
 
     def add_person(self, userid, x, y):
-        x += self.__x_margin
-        y += self.__y_margin
+        x += X_MARGIN
+        y += Y_MARGIN
         symb = shapes.Circle(x, y, 10, color=(50, 150, 255), batch=self.batch)
-        self.__people[userid] = (x, y, symb)
+        
+        self.__people[userid] = (x, y, symb, time.time())
+        self.__remove_old_points()
+
+    def __remove_old_points(self):
+        ct = time.time()
+        self.__people = {key: value for key, value in self.__people.items() if (ct-value[3])< MAX_IDLE_TIME}
 
     def edit_person(self, userid, x, y):
         self.__people[userid].x = x
@@ -54,13 +66,20 @@ class PeopleTrackingUI:
                         data = conn.recv(1024).decode('utf-8')
                         if not data:
                             break
+                        #print(f"Received: {data}")
+                        while '<END>' in data:
+                            message, data = data.split('<END>', 1)
+                            message = message.strip()
+                            data = data.strip()
+                            userid, str_x, str_y = message.split(sep=' ')
 
-                        userid, str_x, str_y = data.split(sep=' ')
+                            map_x = int(float(str_x) * PIXEL_PER_METER )
+                            map_y = int(float(str_y) * PIXEL_PER_METER )
 
-                        # Creare un oggetto nel thread principale di Pyglet
-                        pyglet.clock.schedule_once(lambda dt: self.add_person(userid, int(str_x), int(str_y)), 0)
-                        conn.sendall(f"Oggetto creato per comando: {data}".encode('utf-8'))
-
+                            # Creare un oggetto nel thread principale di Pyglet
+                            self.add_person(userid, map_x, map_y)
+                            #pyglet.clock.schedule_once(lambda dt: self.add_person(userid, map_x, map_y), 0)
+                            conn.sendall(f"Oggetto creato per comando: {message}".encode('utf-8'))
 
     def run(self):
 
@@ -74,5 +93,5 @@ class PeopleTrackingUI:
 
 
 if __name__ == "__main__":
-    ui = PeopleTrackingUI(640, 480)
+    ui = PeopleTrackingUI(16, 16)
     ui.run()

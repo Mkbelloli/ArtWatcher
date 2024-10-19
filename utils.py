824a766f3b6320cc5fa5665bdda8e062b23a9808
m.belloli <michele.belloli@gmail.com>
2024-10-19 04:12:22 +0200

    Added comments

--- utils.py
+++ utils.py
@@ -1,15 +1,30 @@
+
 log_flag = False
 
 def activate_logs():
+    """
+    activate logs
+    :return:
+    """
     global log_flag
     log_flag = True
 
 
 def deactivate_logs():
+    """
+    deactivate logs
+    :return:
+    """
     global log_flag
     log_flag = False
 
 def print_log(msg):
+    """
+    print message according activation log
+    :param msg: message to be printed
+    :return:
+    """
     global log_flag
     if log_flag:
         print(msg)
+
